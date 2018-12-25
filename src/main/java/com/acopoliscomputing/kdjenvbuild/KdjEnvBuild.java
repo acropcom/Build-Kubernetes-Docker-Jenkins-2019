5e1cdfa215b40ad821fe9b29ea59ec3173ecdca5
William Goltz <william_goltz@yahoo.com>
2018-12-25 16:09:32 -0600

    Code Added

---
+++ src/main/java/com/acopoliscomputing/kdjenvbuild/KdjEnvBuild.java
@@ -0,0 +1,14 @@
+package com.acopoliscomputing.kdjenvbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdjEnvBuild {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdjEnvBuild.class, args);
+	}
+
+}
+
